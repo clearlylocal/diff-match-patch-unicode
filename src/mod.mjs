50d487920efe83158b6082c9d30187749ad63c1e
lionel-rowe <lionel.rowe@gmail.com>
2024-03-28 18:10:20 +0800

    Move segment encoding logic to own class

--- src/mod.mjs
+++ src/mod.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 
-import { Diff, diff_match_patch, MAX_BMP_CODEPOINT, TWO_THIRDS_OF_MAX_BMP_CODEPOINT } from './_diff_match_patch.mjs'
+import {
+	Diff,
+	diff_match_patch,
+	DiffOperation,
+	MAX_BMP_CODEPOINT,
+	TWO_THIRDS_OF_MAX_BMP_CODEPOINT,
+} from './_diff_match_patch.mjs'
 export { Diff, DiffOperation, Patch } from './_diff_match_patch.mjs'
 
 /**
@@ -22,14 +28,6 @@ export const segmenters = /** @satisfies {Record<String, SimpleSegmenter>} */ ({
 const defaultDiffOptions = {
 	segmenter: segmenters.char,
 }
-
-/**
- * @typedef {{
- * 	n: number,
- * 	map: Map<string, string>,
- * 	revMap: Map<string, string>,
- * }} EncodingState
- */
 
 export class DiffMatchPatch extends diff_match_patch {
 	/**
@@ -66,56 +64,70 @@ export class DiffMatchPatch extends diff_match_patch {
 	 * ```
 	 */
 	diff(str1, str2, options) {
+		if (str1 === str2) {
+			// no need to encode if both strings are the same
+			return str1 ? [new Diff(DiffOperation.Equal, str1)] : []
+		}
+
 		const { segmenter } = { ...defaultDiffOptions, ...options }
+
+		const encoder = new SegmentEncoder()
 
 		/** @type {SimpleSegmenter} */
 		const segment = segmenter instanceof Intl.Segmenter
 			? (str) => [...segmenter.segment(str)].map((x) => x.segment)
 			: segmenter
 
-		/** @type {EncodingState} */
-		const state = { n: 0, map: new Map(), revMap: new Map() }
-
-		const chars1 = this.#encode(segment(str1), state, TWO_THIRDS_OF_MAX_BMP_CODEPOINT)
-		const chars2 = this.#encode(segment(str2), state, MAX_BMP_CODEPOINT)
+		const chars1 = encoder.encode(segment(str1), TWO_THIRDS_OF_MAX_BMP_CODEPOINT)
+		const chars2 = encoder.encode(segment(str2), MAX_BMP_CODEPOINT)
 
 		const d = this.diff_main(chars1, chars2)
 
-		return d.map(({ op, text }) => new Diff(op, [...text].map((x) => state.revMap.get(x)).join('')))
+		return d.map(({ op, text }) => new Diff(op, [...text].map((x) => encoder.revMap.get(x)).join('')))
 	}
+}
+
+/**
+ * Stateful class for encoding segments as chars
+ */
+class SegmentEncoder {
+	n = 0
+	/** @type {Map<string, string>} */
+	map = new Map()
+	/** @type {Map<string, string>} */
+	revMap = new Map()
 
 	/**
 	 * @param {string[]} segments
-	 * @param {EncodingState} state
 	 * @param {number} max
 	 * @returns {string}
 	 */
-	#encode(segments, state, max) {
+	encode(segments, max) {
 		let out = ''
 		for (let i = 0; i < segments.length; ++i) {
 			const segment = segments[i]
 
-			if (state.map.get(segment) == null) {
-				;++state.n
+			if (this.map.get(segment) == null) {
+				;++this.n
 
-				const char = String.fromCharCode(state.n)
+				const char = String.fromCharCode(this.n)
 
-				if (state.n === max) {
+				if (this.n === max) {
 					const segment = segments.slice(i).join('')
 
-					state.map.set(segment, char)
-					state.revMap.set(char, segment)
+					this.map.set(segment, char)
+					this.revMap.set(char, segment)
 
 					out += char
 
 					break
 				}
 
-				state.map.set(segment, char)
-				state.revMap.set(char, segment)
+				this.map.set(segment, char)
+				this.revMap.set(char, segment)
 			}
 
-			out += state.map.get(segment)
+			out += this.map.get(segment)
 		}
 
 		return out
