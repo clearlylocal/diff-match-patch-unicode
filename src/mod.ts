e66392a8fef51958d13504d33f917f09ea3abd2c
lionel-rowe <lionel.rowe@gmail.com>
2024-09-14 17:49:52 +0800

    v0.0.6

--- src/mod.ts
+++ src/mod.ts
@@ -1,8 +1,2 @@
-/**
- * Modern JS/TS and Unicode-friendly version of [diff-match-patch](https://github.com/google/diff-match-patch).
- *
- * @module
- */
-
 export { Diff, DiffOperation } from './Diff.ts'
 export { Differ, type DiffOptions, segmenters } from './Differ.ts'
